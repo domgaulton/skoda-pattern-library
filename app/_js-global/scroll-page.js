a16e680e4101a25927499425e17a3416a8a67e15
Dom Gaulton <dom.gaulton@readingroom.com>
2018-02-21 17:22:00 +0000

    removing all files other than index

--- app/_js-global/scroll-page.js
+++
@@ -1,7 +0,0 @@
-$('.cta__scroll--light, .cta__scroll--dark').click(function(event){
-	event.preventDefault();
-	$('html, body').animate({
-		scrollTop: $(this).offset().top + 100
-	}, 800);
-	return false;
-});
