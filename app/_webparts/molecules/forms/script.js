d721123f2fe14aff1ced89d12ad8715d22039d0e
Dom Gaulton <dom.gaulton@readingroom.com>
2018-02-26 18:40:21 +0000

    forms update

--- app/_webparts/molecules/forms/script.js
+++ app/_webparts/molecules/forms/script.js
@@ -1,14 +1,29 @@
-$('.field-element input, .field-element textarea').keyup(function(){
-	var element = $(this);
-	var input = element.val();
-	if (input.length > 1) {
-		element.addClass('complete');
-		element.removeClass('error');
-	} else if ( input.length == 0 ) {
-		element.removeClass('complete')
-		element.addClass('error');
-	} else {
-		element.addClass('complete');
-		element.removeClass('error');
-	}
-})
+// $('.field-element input, .field-element textarea').keyup(function(){
+// 	var element = $(this);
+// 	var input = element.val();
+// 	if (input.length > 1) {
+// 		element.addClass('complete');
+// 		element.removeClass('error');
+// 	} else if ( input.length == 0 ) {
+// 		element.removeClass('complete')
+// 		element.addClass('error');
+// 	} else {
+// 		element.addClass('complete');
+// 		element.removeClass('error');
+// 	}
+// })
+
+
+
+
+
+// $('.field-element').children(function(){
+// 	var element = $(this);
+// 	var input = element.val();
+// 	if (input.length > 1) {
+		
+// 	} else {
+// 		element.addClass('complete');
+// 		element.removeClass('error');
+// 	}
+// })
