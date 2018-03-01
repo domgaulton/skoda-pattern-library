8045eefcb7fc25cd9fcf45f31886f3ad32493b45
Dom Gaulton <dom.gaulton@readingroom.com>
2018-03-01 10:10:54 +0000

    updates to javascript

--- app/_js-global/background-image.js
+++ app/_js-global/background-image.js
@@ -1,9 +1,11 @@
-$('[data-bg-image]').each(function() {
-	var src = $(this).attr('data-bg-image');
-	$(this).css('background-image','url('+src+')');
-});
+if ( $('[data-bg-image]').length ) {
+	$('[data-bg-image]').each(function() {
+		var src = $(this).attr('data-bg-image');
+		$(this).css('background-image','url('+src+')');
+	});
 
-$('[data-bg-image-dark]').each(function() {
-	var src = $(this).attr('data-bg-image-dark');
-	$(this).css('background-image','linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('+src+')');
-});
+	$('[data-bg-image-dark]').each(function() {
+		var src = $(this).attr('data-bg-image-dark');
+		$(this).css('background-image','linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('+src+')');
+	});
+};
