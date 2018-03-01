5de794dab8b4668ea127578c72836bfde6efdc47
Dom Gaulton <dom.gaulton@readingroom.com>
2018-03-01 13:11:58 +0000

    navigation styling updated

--- app/_webparts/pattern-library/nav/script.js
+++ app/_webparts/pattern-library/nav/script.js
@@ -1,3 +1,4 @@
 $('.nav-show-hide').click(function(){
 	$('.pattern-library-nav').toggleClass('hide-nav');
+	$('.pattern-library-body').toggleClass('hide-nav');
 })
--- dist/js/script-pattern-library.js
+++ dist/js/script-pattern-library.js
@@ -2,4 +2,5 @@
 
 $('.nav-show-hide').click(function(){
 	$('.pattern-library-nav').toggleClass('hide-nav');
+	$('.pattern-library-body').toggleClass('hide-nav');
 })
