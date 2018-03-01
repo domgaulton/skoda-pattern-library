683a83551607668b5c4b971b5176703ab59937a4
Dom Gaulton <dom.gaulton@readingroom.com>
2018-03-01 15:48:05 +0000

    navigation for pattern library updated

--- app/_webparts/pattern-library/nav/script.js
+++ app/_webparts/pattern-library/nav/script.js
@@ -1,23 +1,49 @@
+// $(document).ready(function(){
+// 	if ( localStorage.navigation == "true" ) {
+// 		console.log('nav-local');
+// 		$('.pattern-library-nav').removeClass('hide-nav');
+// 		$('.pattern-library-body').removeClass('hide-nav');
+// 	} else {
+// 		console.log('not set');
+// 	}
+// });
+
+
+// function navigationShowHide() {
+// 	if ( $('.pattern-library-nav').hasClass('hide-nav') && $('.pattern-library-body').hasClass('hide-nav') ) {
+// 		$('.pattern-library-nav').removeClass('hide-nav');
+// 		$('.pattern-library-body').removeClass('hide-nav');
+// 		localStorage.setItem("navigation", true);
+// 	} else {
+// 		$('.pattern-library-nav').addClass('hide-nav');
+// 		$('.pattern-library-body').addClass('hide-nav');
+// 		localStorage.setItem("navigation", false);
+// 	}
+// }
+
 $('.nav-show-hide').click(function(){
+
+	//navigationShowHide();
+
 	$('.pattern-library-nav').toggleClass('hide-nav');
 	$('.pattern-library-body').toggleClass('hide-nav');
 });
 
-$(window).resize(function() {
-	navDisplay();
-});
-
-// $(document).ready(function(){
+// $(window).resize(function() {
 // 	navDisplay();
 // });
 
-function navDisplay(){
-	if($(window).width() <=1200) {
-		$('.pattern-library-nav').addClass('hide-nav');
-		$('.pattern-library-body').addClass('hide-nav');
-	} else {
-		$('.pattern-library-nav').removeClass('hide-nav');
-		$('.pattern-library-body').removeClass('hide-nav');
+// // $(document).ready(function(){
+// // 	navDisplay();
+// // });
 
-	}
-}
+// function navDisplay(){
+// 	if($(window).width() <=1200) {
+// 		$('.pattern-library-nav').addClass('hide-nav');
+// 		$('.pattern-library-body').addClass('hide-nav');
+// 	} else {
+// 		$('.pattern-library-nav').removeClass('hide-nav');
+// 		$('.pattern-library-body').removeClass('hide-nav');
+
+// 	}
+// }
