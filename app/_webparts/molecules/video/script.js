bce1ee9c1072c7a765fda3d5b3afb4a91796f91e
Dom Gaulton <dom.gaulton@readingroom.com>
2018-03-05 18:31:55 +0000

    added loop as data attribute to video element

--- app/_webparts/molecules/video/script.js
+++ app/_webparts/molecules/video/script.js
@@ -1,41 +1,46 @@
 if ( $('[data-ytID]').length ) {
-  var tag = document.createElement('script');
+	var tag = document.createElement('script');
 
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  var players = [];
-  var videoIds = [];
-  
-  function onYouTubeIframeAPIReady() {
+	var players = [];
+	var videoIds = [];
+
+	function onYouTubeIframeAPIReady() {
 	$('[data-ytID]').each(function(index) {
 		videoIds[index] = $(this).attr('id');
 		var ytID = $('#' + videoIds[index] + '[data-ytID]').attr('data-ytID');
-		players[index] = new YT.Player(videoIds[index], {
-		  height: '100%',
-		  width: '100%',
-		  videoId: ytID,
-		  playerVars: { 
-			'showinfo':0,
-			'controls':0, 
-			'rel':0,
-			'fs':0,
-			'modestbranding':1,
-			'loop':1,
-		  },
-		  events: {
-			'onReady': function(event) {
-				var autoplay = $('#' + videoIds[index] + '[data-ytID]').attr('data-autoplay');
-				if (autoplay == 1) {
-				  event.target.mute();
-				  event.target.playVideo();
+			players[index] = new YT.Player(videoIds[index], {
+				height: '100%',
+				width: '100%',
+				videoId: ytID,
+				playerVars: { 
+					'showinfo':0,
+					'controls':0, 
+					'rel':0,
+					'fs':0,
+					'modestbranding':1,
+					'loop':1,
+				},
+				events: {
+					'onReady': function(event) {
+						var autoplay = $('#' + videoIds[index]).attr('data-video-autoplay');
+						if (autoplay == 1) {
+							event.target.mute();
+							event.target.playVideo();
+						}
+					},
+					onStateChange: function(event) {
+						var loop = $('#' + videoIds[index]).attr('data-video-loop')
+						if (event.data === YT.PlayerState.ENDED && loop == 1 ) {
+							players[index].playVideo(); 
+						}
+					}
 				}
-			}
-		  }
+			});
 		});
-	});
-  }
-
+	}
 }
 
