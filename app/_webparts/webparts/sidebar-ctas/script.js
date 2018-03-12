f6954341dd9deaa8c6198c10fd1e976dce764385
Dom Gaulton <dom.gaulton@readingroom.com>
2018-03-12 16:14:48 +0000

    final updates - script and styles

--- app/_webparts/webparts/sidebar-ctas/script.js
+++ app/_webparts/webparts/sidebar-ctas/script.js
@@ -16,18 +16,15 @@ $(document).ready(function () {
 		} else if ( url.indexOf("karoq") > -1 ) {
 			var td = 'http://www.skoda.co.uk/tools/book-a-test-drive/?source=karoq';
 			var cc = 'http://cc-cloud.skoda-auto.com/gbr/gbr/en-gb?carline=66328';
-		} else if ( url.indexOf("local") > -1 ) {
-			var td = '#td';
-			var cc = '#cc';
 		}
 
-		$('.humanise-sidebar-ctas__link--td > a').attr('href', td);
-		$('.humanise-sidebar-ctas__link--cc > a').attr('href', cc);
-		$('.humanise-sidebar-ctas__link--lt > a').attr('href', lt);
-		$('.humanise-sidebar-ctas__link--br > a').attr('href', br);
-		$('.humanise-sidebar-ctas__link--fr > a').attr('href', fr);
-		$('.humanise-sidebar-ctas__link--fc > a').attr('href', fc);
-		$('.humanise-sidebar-ctas__link--bs > a').attr('href', bs);
+		$('.humanise-sidebar-ctas__icon--td').next().attr('href', td);
+		$('.humanise-sidebar-ctas__icon--cc').next().attr('href', cc);
+		$('.humanise-sidebar-ctas__icon--lt').next().attr('href', lt);
+		$('.humanise-sidebar-ctas__icon--br').next().attr('href', br);
+		$('.humanise-sidebar-ctas__icon--fr').next().attr('href', fr);
+		$('.humanise-sidebar-ctas__icon--fc').next().attr('href', fc);
+		$('.humanise-sidebar-ctas__icon--bs').next().attr('href', bs);
 
 		$('.humanise-sidebar-ctas').mouseenter(function(){
 			$('.humanise-sidebar-ctas__link').addClass('active');
