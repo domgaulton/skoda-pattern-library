eb36804690cde1c7484a3a7b74090a3c6b76eb47
Tijana Miletic <tijana.miletic@readingroom.com>
2018-03-14 14:14:11 +0000

    Quote slider initial code

---
+++ app/_webparts/webparts/quote-slider/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    var flky = new Flickity('#slider1', {		
+      resize: true,
+      contain: false,
+      prevNextButtons: false,
+      pageDots: true,
+      wrapAround: true,
+      draggable: true,
+	  autoPlay: false
+    });
+	
+    var flky = new Flickity('#slider2', {		
+      resize: true,
+      contain: false,
+      prevNextButtons: false,
+      pageDots: true,
+      wrapAround: true,
+      draggable: true,
+	  autoPlay: false
+    });
+	
+    var flky = new Flickity('#slider3', {		
+      resize: true,
+      contain: false,
+      prevNextButtons: false,
+      pageDots: true,
+      wrapAround: true,
+      draggable: true,
+	  autoPlay: false
+    });
+	
+});
