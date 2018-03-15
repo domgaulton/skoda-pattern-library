06572c26672e0b66a49f32c88f1cd630d5cbd5fc
Dom Gaulton <dom.gaulton@readingroom.com>
2018-03-15 10:51:11 +0000

    WIP - appending controls where html doesn't state no controls.

--- app/_webparts/molecules/video/script.js
+++ app/_webparts/molecules/video/script.js
@@ -1,4 +1,4 @@
-if ( $('[data-ytID]').length ) {
+if ( $('[data-video-ytID]').length ) {
 	var tag = document.createElement('script');
 
 	tag.src = "https://www.youtube.com/iframe_api";
@@ -7,29 +7,50 @@ if ( $('[data-ytID]').length ) {
 
 	var players = [];
 	var videoIds = [];
+	var controls = [];
 
 	function onYouTubeIframeAPIReady() {
-	$('[data-ytID]').each(function(index) {
+	$('[data-video-ytID]').each(function(index) {
 		videoIds[index] = $(this).attr('id');
-		var ytID = $('#' + videoIds[index] + '[data-ytID]').attr('data-ytID');
+		
+		if ( $(this).attr('data-video-controls') == 0 ) {
+			controls[index] = 0 
+		} else {
+			controls[index] = 1;
+		};
+
+		var ytID = $('#' + videoIds[index]).attr('data-video-ytID');
 			players[index] = new YT.Player(videoIds[index], {
 				height: '100%',
 				width: '100%',
 				videoId: ytID,
 				playerVars: { 
 					'showinfo':0,
-					'controls':0, 
+					'controls':controls[index], 
 					'rel':0,
 					'fs':0,
 					'modestbranding':1,
 				},
 				events: {
 					'onReady': function(event) {
-						console.log(event);
+						// console.log(event);
 						var autoplay = $('#' + videoIds[index]).attr('data-video-autoplay');
+						
 						if (autoplay == 1) {
 							event.target.mute();
 							event.target.playVideo();
+						}
+
+						if (controls[index] == 1) {
+							// show controls
+							// console.log(players[index]);
+							// console.log(event.target);
+							// players[index].iframe.classList.addClass('controlsABC');
+							
+							var controlsHTML = "<div class='controls'></div>"
+							$('#' + videoIds[index]).parent().prepend(controlsHTML);
+
+							//$('#' + videoIds[index]).addClass('controls');
 						}
 					},
 					onStateChange: function(event) {
