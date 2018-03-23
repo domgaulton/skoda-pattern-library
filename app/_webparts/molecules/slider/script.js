6952407df02e6fe75d8ee512245e58b8d54230de
Tijana Miletic <tijana.miletic@readingroom.com>
2018-03-23 15:25:28 +0000

    Slider - 1 and 2 item options with breakpoint changes

--- app/_webparts/molecules/slider/script.js
+++ app/_webparts/molecules/slider/script.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
 	
 	if ($('#slider1').length) {
 		
-		var flkty = new Flickity('#slider1', {		
+		var twoCellCarousel = new Flickity('#slider1', {		
 			resize: true,
 			contain: false,
-			groupCells: true,
+			groupCells: 2,
 			prevNextButtons: true,
 			pageDots: false,
 			wrapAround: true,
@@ -13,14 +13,59 @@ $(document).ready(function(){
 			autoPlay: false,
 			on: {
 				ready: function() {
-					$(".flickity-button").wrapAll("<div class='flickity-buttons'></div>");
-					var imageHeight = $("#slider1 .humanise-slide__img").height();
-					$(".flickity-buttons").height(imageHeight);
+					var sliderId = '#' + this.element.id;
+					$(sliderId + " .flickity-button").wrapAll("<div class='flickity-buttons'></div>");
+					var imageHeight = $(sliderId + " .humanise-slide__img").height();
+					$(sliderId + " .flickity-buttons").height(imageHeight);					
 					
 					$(window).resize(function() {
-					  imageHeight = $("#slider1 .humanise-slide__img").height();
-					  $(".flickity-buttons").height(imageHeight);
+					  imageHeight = $(sliderId + " .humanise-slide__img").height();
+					  $(sliderId + " .flickity-buttons").height(imageHeight);
+					  resetGroupCells(twoCellCarousel);
 					});
+					
+					function resetGroupCells(carousel) {
+						if (window.matchMedia('screen and (max-width: 768px)').matches) {
+							carousel.options.groupCells = 1;
+							console.log(carousel.options.groupCells);
+							
+						} else {
+							carousel.options.groupCells = 2;
+							console.log(carousel.options.groupCells);
+						}						
+					}
+					
+					resetGroupCells(this);
+					this.resize();					
+				}
+			}
+		});
+	
+	}
+	
+	if ($('#slider2').length) {
+		
+		var oneCellCarousel = new Flickity('#slider2', {		
+			resize: true,
+			contain: false,
+			groupCells: 1,
+			prevNextButtons: true,
+			pageDots: false,
+			wrapAround: true,
+			draggable: true,
+			autoPlay: false,
+			on: {
+				ready: function() {
+					var sliderId = '#' + this.element.id;
+					$(sliderId + " .flickity-button").wrapAll("<div class='flickity-buttons'></div>");
+					var imageHeight = $(sliderId + " .humanise-slide__img").height();
+					$(sliderId + " .flickity-buttons").height(imageHeight);
+										
+					$(window).resize(function() {
+					  imageHeight = $(sliderId + " .humanise-slide__img").height();
+					  $(sliderId + " .flickity-buttons").height(imageHeight);
+					});
+										
 				}
 			}
 		});
