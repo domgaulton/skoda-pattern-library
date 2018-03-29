5741bdcb1ff181985b9b649312844de64f4b843a
Dom Gaulton <dom.gaulton@readingroom.com>
2018-03-29 16:21:59 +0100

    updated slider as it wasn't dynamic

--- app/_webparts/molecules/slider/script.js
+++ app/_webparts/molecules/slider/script.js
@@ -1,77 +1,71 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 	
-	if ($('#slider1').length) {
+// 	if ($('.humanise-slider__list-item').length) {
 		
-		var twoCellCarousel = new Flickity('#slider1', {		
-			resize: true,
-			contain: false,
-			groupCells: 2,
-			prevNextButtons: true,
-			pageDots: false,
-			wrapAround: true,
-			draggable: true,
-			autoPlay: false,
-			on: {
-				ready: function() {
-					var sliderId = '#' + this.element.id;
-					$(sliderId + " .flickity-button").wrapAll("<div class='flickity-buttons'></div>");
-					var imageHeight = $(sliderId + " .humanise-slide__img").height();
-					$(sliderId + " .flickity-buttons").height(imageHeight);					
+// 		var twoCellCarousel = new Flickity('#slider1', {		
+// 			resize: true,
+// 			contain: false,
+// 			groupCells: 2,
+// 			prevNextButtons: true,
+// 			pageDots: false,
+// 			wrapAround: true,
+// 			draggable: true,
+// 			autoPlay: false,
+// 			on: {
+// 				ready: function() {
+// 					var sliderId = '#' + this.element.id;
+// 					$(sliderId + " .flickity-button").wrapAll("<div class='flickity-buttons'></div>");
+// 					var imageHeight = $(sliderId + " .humanise-slide__img").height();
+// 					$(sliderId + " .flickity-buttons").height(imageHeight);					
 					
-					$(window).resize(function() {
-					  imageHeight = $(sliderId + " .humanise-slide__img").height();
-					  $(sliderId + " .flickity-buttons").height(imageHeight);
-					  resetGroupCells(twoCellCarousel);
-					});
+// 					$(window).resize(function() {
+// 					  imageHeight = $(sliderId + " .humanise-slide__img").height();
+// 					  $(sliderId + " .flickity-buttons").height(imageHeight);
+// 					  resetGroupCells(twoCellCarousel);
+// 					});
 					
-					function resetGroupCells(carousel) {
-						if (window.matchMedia('screen and (max-width: 768px)').matches) {
-							carousel.options.groupCells = 1;
-							console.log(carousel.options.groupCells);
+// 					function resetGroupCells(carousel) {
+// 						if (window.matchMedia('screen and (max-width: 768px)').matches) {
+// 							carousel.options.groupCells = 1;
+// 							console.log(carousel.options.groupCells);
 							
-						} else {
-							carousel.options.groupCells = 2;
-							console.log(carousel.options.groupCells);
-						}						
-					}
+// 						} else {
+// 							carousel.options.groupCells = 2;
+// 							console.log(carousel.options.groupCells);
+// 						}						
+// 					}
 					
-					resetGroupCells(this);
-					this.resize();					
-				}
-			}
-		});
-	
-	}
-	
-	if ($('#slider2').length) {
-		
-		var oneCellCarousel = new Flickity('#slider2', {		
-			resize: true,
-			contain: false,
-			groupCells: 1,
-			prevNextButtons: true,
-			pageDots: false,
-			wrapAround: true,
-			draggable: true,
-			autoPlay: false,
-			on: {
-				ready: function() {
-					var sliderId = '#' + this.element.id;
-					$(sliderId + " .flickity-button").wrapAll("<div class='flickity-buttons'></div>");
-					var imageHeight = $(sliderId + " .humanise-slide__img").height();
-					$(sliderId + " .flickity-buttons").height(imageHeight);
+// 					resetGroupCells(this);
+// 					this.resize();					
+// 				}
+// 			}
+// 		});
+
+// 		var oneCellCarousel = new Flickity('#slider2', {		
+// 			resize: true,
+// 			contain: false,
+// 			groupCells: 1,
+// 			prevNextButtons: true,
+// 			pageDots: false,
+// 			wrapAround: true,
+// 			draggable: true,
+// 			autoPlay: false,
+// 			on: {
+// 				ready: function() {
+// 					var sliderId = '#' + this.element.id;
+// 					$(sliderId + " .flickity-button").wrapAll("<div class='flickity-buttons'></div>");
+// 					var imageHeight = $(sliderId + " .humanise-slide__img").height();
+// 					$(sliderId + " .flickity-buttons").height(imageHeight);
 										
-					$(window).resize(function() {
-					  imageHeight = $(sliderId + " .humanise-slide__img").height();
-					  $(sliderId + " .flickity-buttons").height(imageHeight);
-					});
+// 					$(window).resize(function() {
+// 					  imageHeight = $(sliderId + " .humanise-slide__img").height();
+// 					  $(sliderId + " .flickity-buttons").height(imageHeight);
+// 					});
 										
-				}
-			}
-		});
-	
-	}
-	
-});
+// 				}
+// 			}
+// 		});
+// 	}	
+// });
 
 
--- app/_webparts/webparts/quote-slider/script.js
+++ app/_webparts/webparts/quote-slider/script.js
@@ -1,39 +1,57 @@
 $(document).ready(function(){
-	
-	if ($('#slider1').length) {
-		var flkty = new Flickity('#slider1', {		
-		  resize: true,
-		  contain: false,
-		  prevNextButtons: false,
-		  pageDots: true,
-		  wrapAround: true,
-		  draggable: true,
-		  autoPlay: false
-		});
+
+	if ($('.humanise-slider-content').length) {
+
+		$('.humanise-slider-content').each(function(index){
+			var sliderID = "slider"+index;
+			$(this).attr("id", sliderID);
+
+			var flkty = new Flickity("#"+sliderID, {		
+			  resize: true,
+			  contain: false,
+			  prevNextButtons: false,
+			  pageDots: true,
+			  wrapAround: true,
+			  draggable: true,
+			  autoPlay: false
+			});
+		})		
 	}
+
+	// if ($('#slider1').length) {
+	// 	var flkty = new Flickity('#slider1', {		
+	// 	  resize: true,
+	// 	  contain: false,
+	// 	  prevNextButtons: false,
+	// 	  pageDots: true,
+	// 	  wrapAround: true,
+	// 	  draggable: true,
+	// 	  autoPlay: false
+	// 	});
+	// }
 	
-	if ($('#slider2').length) {
-		var flkty = new Flickity('#slider2', {		
-		  resize: true,
-		  contain: false,
-		  prevNextButtons: false,
-		  pageDots: true,
-		  wrapAround: true,
-		  draggable: true,
-		  autoPlay: false
-		});
-	}
+	// if ($('#slider2').length) {
+	// 	var flkty = new Flickity('#slider2', {		
+	// 	  resize: true,
+	// 	  contain: false,
+	// 	  prevNextButtons: false,
+	// 	  pageDots: true,
+	// 	  wrapAround: true,
+	// 	  draggable: true,
+	// 	  autoPlay: false
+	// 	});
+	// }
 	
-	if ($('#slider3').length) {
-		var flkty = new Flickity('#slider3', {		
-		  resize: true,
-		  contain: false,
-		  prevNextButtons: false,
-		  pageDots: true,
-		  wrapAround: true,
-		  draggable: true,
-		  autoPlay: false
-		});
-	}
-	
+	// if ($('#slider3').length) {
+	// 	var flkty = new Flickity('#slider3', {		
+	// 	  resize: true,
+	// 	  contain: false,
+	// 	  prevNextButtons: false,
+	// 	  pageDots: true,
+	// 	  wrapAround: true,
+	// 	  draggable: true,
+	// 	  autoPlay: false
+	// 	});
+	// }
+
 });
