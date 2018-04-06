590a87167716442ae0dc7e1c022d32e59dea1075
Tijana Miletic <tijana.miletic@readingroom.com>
2018-04-06 10:01:51 +0100

    Slider amends

--- app/_webparts/webparts/quote/script.js
+++ app/_webparts/webparts/quote/script.js
@@ -1,57 +1,19 @@
 $(document).ready(function(){
-
-	if ($('.humanise-slider-content').length) {
-
-		$('.humanise-slider-content').each(function(index){
-			var sliderID = "slider"+index;
-			$(this).attr("id", sliderID);
-
-			var flkty = new Flickity("#"+sliderID, {		
-			  resize: true,
-			  contain: false,
-			  prevNextButtons: false,
-			  pageDots: true,
-			  wrapAround: true,
-			  draggable: true,
-			  autoPlay: false
+	
+	if ($('.humanise-quote-slider__list').length) {
+		var sliderItem = document.querySelectorAll('.humanise-quote-slider__list');
+		for (var i=0, numberOfItems = sliderItem.length; i < numberOfItems; i++) {
+			var currentSliderItem = sliderItem[i];
+			new Flickity(currentSliderItem, {
+				resize: true,
+				contain: false,
+				prevNextButtons: false,
+				pageDots: true,
+				wrapAround: true,
+				draggable: true,
+				autoPlay: false
 			});
-		})		
+		}
 	}
-
-	// if ($('#slider1').length) {
-	// 	var flkty = new Flickity('#slider1', {		
-	// 	  resize: true,
-	// 	  contain: false,
-	// 	  prevNextButtons: false,
-	// 	  pageDots: true,
-	// 	  wrapAround: true,
-	// 	  draggable: true,
-	// 	  autoPlay: false
-	// 	});
-	// }
 	
-	// if ($('#slider2').length) {
-	// 	var flkty = new Flickity('#slider2', {		
-	// 	  resize: true,
-	// 	  contain: false,
-	// 	  prevNextButtons: false,
-	// 	  pageDots: true,
-	// 	  wrapAround: true,
-	// 	  draggable: true,
-	// 	  autoPlay: false
-	// 	});
-	// }
-	
-	// if ($('#slider3').length) {
-	// 	var flkty = new Flickity('#slider3', {		
-	// 	  resize: true,
-	// 	  contain: false,
-	// 	  prevNextButtons: false,
-	// 	  pageDots: true,
-	// 	  wrapAround: true,
-	// 	  draggable: true,
-	// 	  autoPlay: false
-	// 	});
-	// }
-
 });
