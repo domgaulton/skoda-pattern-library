033c565aa9f5dabbd3a6bf0a988ccd4f21e29b47
Dom Gaulton <dom.gaulton@readingroom.com>
2018-04-09 16:43:55 +0100

    Range header updated
    js data-bg-image selector, forgot the -dark selector too

--- app/_js-global/background-image.js
+++ app/_js-global/background-image.js
@@ -1,4 +1,4 @@
-if ( $('[data-bg-image]').length ) {
+if ( $('[data-bg-image]').length || $('[data-bg-image-dark]').length ) {
 	$('[data-bg-image]').each(function() {
 		var src = $(this).attr('data-bg-image');
 		$(this).css('background-image','url('+src+')');
