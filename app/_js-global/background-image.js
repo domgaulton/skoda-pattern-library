8fb35f141574ad08381df2fce8196b0ae7f49ba4
Dom Gaulton <dom.gaulton@readingroom.com>
2018-04-10 15:28:29 +0100

    Octavia Family Page

--- app/_js-global/background-image.js
+++ app/_js-global/background-image.js
@@ -1,11 +1,16 @@
-if ( $('[data-bg-image]').length || $('[data-bg-image-dark]').length ) {
+if ( $('[data-bg-image]').length || $('[data-bg-image-dark-dark]').length || $('[data-bg-image-dark-light]').length ) {
 	$('[data-bg-image]').each(function() {
 		var src = $(this).attr('data-bg-image');
 		$(this).css('background-image','url('+src+')');
 	});
 
-	$('[data-bg-image-dark]').each(function() {
-		var src = $(this).attr('data-bg-image-dark');
+	$('[data-bg-image-dark-light]').each(function() {
+		var src = $(this).attr('data-bg-image-dark-light');
 		$(this).css('background-image','linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) ), url('+src+')');
 	});
+
+    $('[data-bg-image-dark-dark]').each(function() {
+    var src = $(this).attr('data-bg-image-dark-dark');
+    $(this).css('background-image','linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ), url('+src+')');
+  });
 };
