a5882693e49ee236a7d980beedf8c170b24bfac2
Dom Gaulton <dom.gaulton@readingroom.com>
2018-04-12 13:51:23 +0100

    prevent default on scroll click

--- app/_webparts/molecules/ctas/script.js
+++ app/_webparts/molecules/ctas/script.js
@@ -1,7 +1,8 @@
 $('.cta-scroll').click(function(){
+  event.preventDefault();
   var offset = $(this).offset().top;
   window.scrollBy({ 
-    top: offset, // could be negative value
+    top: offset + 100, // could be negative value
     left: 0, 
     behavior: 'smooth' 
   });
